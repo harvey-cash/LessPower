c02e9d0a36c279f765e40176201006054526cbad
HOCgaming <HCash1@sheffield.ac.uk>
2017-11-25 13:24:56 +0000

    Basic Rolling

--- PowerLess/Assets/Scripts/InputController.cs
+++ PowerLess/Assets/Scripts/InputController.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputController : MonoBehaviour {
+public class InputController  {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        if (Input.GetButtonDown()) {
-
+	public Vector3 GetInput() {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+            return Vector3.left;
         }
-		
-	}
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+            return Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+            return Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+            return Vector3.back;
+        }
+
+        return Vector3.zero;
+    }
 }
--- PowerLess/Assets/Scripts/PlayerController.cs
+++ PowerLess/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,71 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public void Move(Vector3 direction) {
-        transform.Translate(direction);
+    private InputController inputController = new InputController();
+
+    private bool canMove = true;
+    private float rollSpeed = 250;
+
+    private void Update() {        
+        Move();
+    }
+
+    /*
+     * For a given direction, roll the player.
+     */
+    private void Move() {
+        if (canMove) {
+            Vector3 direction = inputController.GetInput();
+
+            if (direction != Vector3.zero) {
+                StartCoroutine(Roll(transform.position, direction));
+            }            
+        }
+    }
+
+    /*
+     * Begin rolling, prevent any further movement until roll
+     * is complete.
+     */
+    private IEnumerator Roll(Vector3 start, Vector3 direction) {
+        canMove = false;
+        Quaternion startRotation = transform.rotation;
+        
+        Vector3 rollPoint = 
+            (transform.position - (new Vector3(0, transform.localScale.y / 2, 0))) 
+            + (direction / 2);
+        Vector3 rollAxis = RollAxis(direction);
+
+        float completion = 0;
+        while (completion < 90) {
+            float angle = Time.deltaTime * rollSpeed;
+            transform.RotateAround(rollPoint, rollAxis, angle);
+            completion += angle;
+            yield return new WaitForEndOfFrame();
+        }
+
+        // Reset to uniform alignment. No actual roll happened, guys!        
+        transform.rotation = startRotation;
+        transform.RotateAround(rollPoint, rollAxis, 90);
+        transform.position = start + direction;
+        canMove = true;
+    }
+
+    /*
+     * For the given direction, return the correct roll axis.
+     */ 
+     private Vector3 RollAxis(Vector3 direction) {
+        if (direction.normalized == Vector3.forward) {
+            return Vector3.right;
+        }
+        else if (direction.normalized == Vector3.right) {
+            return Vector3.back;
+        }
+        else if (direction.normalized == Vector3.back) {
+            return Vector3.left;
+        }
+        else {
+            return Vector3.forward;
+        }
     }
 }
